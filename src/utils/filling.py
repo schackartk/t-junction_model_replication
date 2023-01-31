864b4f691faef093f1b35f461566e4e91744cf1e
Kenneth Schackart <schackartk1@gmail.com>
2023-01-30 20:54:12 -0700

    add functions using incorrect equations for filling

--- src/utils/filling.py
+++ src/utils/filling.py
@@ -212,6 +212,78 @@ def test_calc_fill_volume() -> None:
 
 
 # -------------------------------------------------------------------------------------
+def calc_incorrect_fill_volume(
+    height: float, width: float, inlet_width: float
+) -> float:
+    """
+    Calculate the filling volume using modified (incorrect) equation to reproduce
+    figure 2a exactly.
+
+    Arguments:
+    `height`: channel height
+    `width`: channel width
+    `inlet_width`: inlet channel width
+    """
+
+    if inlet_width <= width:
+        # No mistake in equation, use correct version
+        fill_volume = calc_fill_volume(height, width, inlet_width)
+
+    else:
+        # First and second terms are correct
+        first_term = ((PI / 4) - 0.5 * math.asin(1 - (width / inlet_width))) * (
+            inlet_width / width
+        ) ** 2
+
+        second_term = (-0.5) * ((inlet_width / width) - 1) * (
+            2 * (inlet_width / width) - 1
+        ) ** 0.5 + (PI / 8)
+
+        # Third term is missing parentheses
+        third_term = (
+            (-0.5)
+            * (1 - (PI / 4))
+            * (
+                (PI / 2)
+                - math.asin(1 - (width / inlet_width)) * (inlet_width / width)
+                + (PI / 2)
+            )
+            * (height / width)
+        )
+
+        nondim_fill_volume = first_term + second_term + third_term
+        fill_volume = nondim_fill_volume * (height * width**2)
+
+    return fill_volume
+
+
+# -------------------------------------------------------------------------------------
+def test_calc_incorrect_fill_volume() -> None:
+    """Test calc_incorrect_fill_volume()"""
+
+    height = 10.0
+    width = 20.0
+
+    # Functions are same if inlet_width <= width
+    assert calc_fill_volume(height, width, width - 2.0) == calc_incorrect_fill_volume(
+        height, width, width - 2.0
+    )
+    assert calc_fill_volume(height, width, width) == calc_incorrect_fill_volume(
+        height, width, width
+    )
+
+    # Functions are different if inlet_width > width
+    assert calc_fill_volume(height, width, width * 2.0) != calc_incorrect_fill_volume(
+        height, width, width * 2.0
+    )
+
+    # Incorrect function gives volume greater than should to be
+    assert calc_fill_volume(height, width, width * 2.0) < calc_incorrect_fill_volume(
+        height, width, width * 2.0
+    )
+
+
+# -------------------------------------------------------------------------------------
 def calc_nondim_fill_volume(
     height: float, width: float, inlet_width: float
 ) -> Optional[float]:
@@ -281,3 +353,53 @@ def test_calc_nondim_fill_volume() -> None:
     assert calc_nondim_fill_volume(0.0, 1.0, 1.0) is None
     assert calc_nondim_fill_volume(1.0, 0.0, 1.0) is None
     assert calc_nondim_fill_volume(1.0, 1.0, 0.0) is None
+
+
+# -------------------------------------------------------------------------------------
+def calc_incorrect_nondim_fill_volume(
+    height: float, width: float, inlet_width: float
+) -> Optional[float]:
+    """
+    Calculate the non-dimensionalized fill volume using the incorrect equation
+    in order to reproduce figure 2a exactly
+
+    Arguments:
+    `height`: channel height
+    `width`: channel width
+    `inlet_width`: inlet channel width
+    """
+
+    if 0 in [height, width, inlet_width]:
+        return None
+
+    fill_volume = calc_incorrect_fill_volume(height, width, inlet_width)
+
+    non_dim_volume = fill_volume / (height * width**2)
+
+    return non_dim_volume
+
+
+# -------------------------------------------------------------------------------------
+def test_calc_incorrect_nondim_fill_volume() -> None:
+    """Test calc_incorrect_nondim_fill_volume()"""
+
+    height = 10.0
+    width = 20.0
+
+    # Functions are same if inlet_width <= width
+    assert calc_nondim_fill_volume(
+        height, width, width - 2.0
+    ) == calc_incorrect_nondim_fill_volume(height, width, width - 2.0)
+    assert calc_nondim_fill_volume(
+        height, width, width
+    ) == calc_incorrect_nondim_fill_volume(height, width, width)
+
+    # Functions are different if inlet_width > width
+    assert calc_nondim_fill_volume(
+        height, width, width * 2.0
+    ) != calc_incorrect_nondim_fill_volume(height, width, width * 2.0)
+
+    # Incorrect function gives volume greater than should to be
+    assert calc_nondim_fill_volume(
+        height, width, width * 2.0
+    ) < calc_incorrect_nondim_fill_volume(height, width, width * 2.0)
