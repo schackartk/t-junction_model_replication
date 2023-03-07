96d9a2f3301c7800c028b4a02161cb807a2f8c22
Kenneth Schackart <schackartk1@gmail.com>
2023-03-07 14:35:07 -0700

    rename variabels

--- src/make_figures.py
+++ src/make_figures.py
@@ -19,7 +19,17 @@ from t_junction_model.filling import (
 )
 from t_junction_model.squeezing import _calc_2r, _calc_alpha
 from t_junction_model.total import calc_nondim_total_volume
-from utils.formatter_class import CustomHelpFormatter
+from formatters.formatter_class import CustomHelpFormatter
+
+# Dictionary mapping inlet width / channel with ratio to color
+COLOR_MAPPING = {
+    "0.33": "#CF232B",
+    "0.67": "#CF232B",
+    "1.0": "#CF232B",
+    "1.33": "#CF232B",
+    "2.0": "#CF232B",
+    "3.0": "#CF232B",
+}
 
 
 class Args(NamedTuple):
@@ -79,34 +89,30 @@ def make_fig_2a(color_mapping: dict[str, str], filling_function: Callable) -> p9
         height_col.append(height)
         inlet_width_col.append(inlet_width)
 
-    filling_df = pd.DataFrame()
-    filling_df["width"] = width_col
-    filling_df["height"] = height_col
-    filling_df["inlet_width"] = inlet_width_col
+    df = pd.DataFrame()
+    df["width"] = width_col
+    df["height"] = height_col
+    df["inlet_width"] = inlet_width_col
 
-    filling_df["height_over_width"] = filling_df["height"] / filling_df["width"]
-    filling_df["width_ratio"] = round(
-        filling_df["inlet_width"] / filling_df["width"], 2
-    )
-    filling_df["width_ratio"] = filling_df["width_ratio"].astype(str)
-    filling_df["width_ratio_labs"] = filling_df.apply(
-        lambda row: "w_in/w=" + row.width_ratio, axis=1
-    )
+    df["height_over_width"] = df["height"] / df["width"]
+    df["width_ratio"] = round(df["inlet_width"] / df["width"], 2)
+    df["width_ratio"] = df["width_ratio"].astype(str)
+    df["width_ratio_labs"] = df.apply(lambda row: "w_in/w=" + row.width_ratio, axis=1)
 
-    filling_df["nondim_vol"] = filling_df.apply(
+    df["nondim_vol"] = df.apply(
         lambda row: filling_function(row.height, row.width, row.inlet_width),
         axis=1,
     )
 
     plot = (
         p9.ggplot(
-            filling_df,
+            df,
             p9.aes("height_over_width", "nondim_vol", color="width_ratio"),
         )
         + p9.geom_line()
         + p9.scale_color_manual(color_mapping)
         + p9.geom_label(
-            filling_df[filling_df["height_over_width"] == 0.25],
+            df[df["height_over_width"] == 0.25],
             p9.aes(
                 label="width_ratio_labs",
             ),
@@ -137,7 +143,7 @@ def make_fig_2b(color_mapping: dict[str, str]) -> p9.ggplot:
 
     widths = [1.0]
     flow_ratio = 0.1
-    epsilon = 0.0
+    corner_roundness = 0.0
     heights = map(lambda x: float(x / 2000), range(1, 1001))
     inlet_widths = [1 / 3, 2 / 3, 1, 4 / 3, 2, 3]
 
@@ -149,27 +155,25 @@ def make_fig_2b(color_mapping: dict[str, str]) -> p9.ggplot:
         height_col.append(height)
         inlet_width_col.append(inlet_width)
 
-    alpha_df = pd.DataFrame()
-    alpha_df["width"] = width_col
-    alpha_df["height"] = height_col
-    alpha_df["inlet_width"] = inlet_width_col
-    alpha_df["epsilon"] = epsilon
-    alpha_df["flow_cont"] = 1.0
-    alpha_df["flow_gutter"] = alpha_df["flow_cont"] * flow_ratio
+    df = pd.DataFrame()
+    df["width"] = width_col
+    df["height"] = height_col
+    df["inlet_width"] = inlet_width_col
+    df["corner_roundness"] = corner_roundness
+    df["flow_cont"] = 1.0
+    df["flow_gutter"] = df["flow_cont"] * flow_ratio
 
-    alpha_df["height_over_width"] = alpha_df["height"] / alpha_df["width"]
-    alpha_df["width_ratio"] = round(alpha_df["inlet_width"] / alpha_df["width"], 2)
-    alpha_df["width_ratio"] = alpha_df["width_ratio"].astype(str)
-    alpha_df["width_ratio_labs"] = alpha_df.apply(
-        lambda row: "w_in/w=" + row.width_ratio, axis=1
-    )
+    df["height_over_width"] = df["height"] / df["width"]
+    df["width_ratio"] = round(df["inlet_width"] / df["width"], 2)
+    df["width_ratio"] = df["width_ratio"].astype(str)
+    df["width_ratio_labs"] = df.apply(lambda row: "w_in/w=" + row.width_ratio, axis=1)
 
-    alpha_df["alpha"] = alpha_df.apply(
+    df["alpha"] = df.apply(
         lambda row: _calc_alpha(
             row.height,
             row.width,
             row.inlet_width,
-            row.epsilon,
+            row.corner_roundness,
             row.flow_cont,
             row.flow_gutter,
         ),
@@ -177,12 +181,12 @@ def make_fig_2b(color_mapping: dict[str, str]) -> p9.ggplot:
     )
 
     plot = (
-        p9.ggplot(alpha_df, p9.aes("height_over_width", "alpha", color="width_ratio"))
+        p9.ggplot(df, p9.aes("height_over_width", "alpha", color="width_ratio"))
         + p9.geom_line()
         + p9.scale_y_continuous(breaks=list(range(0, 9, 1)), limits=[0, 8])
         + p9.scale_color_manual(color_mapping)
         + p9.geom_label(
-            alpha_df[alpha_df["height_over_width"] == 0.25],
+            df[df["height_over_width"] == 0.25],
             p9.aes(
                 label="width_ratio_labs",
             ),
@@ -230,21 +234,19 @@ def make_fig_3(color_mapping: dict[str, str]) -> p9.ggplot:
         dispersed_flow_col.append(dispersed_flow)
 
     # Parameters for bubbles
-    vol_df = pd.DataFrame()
-    vol_df["inlet_width"] = inlet_width_col
-    vol_df["dispersed_flow"] = dispersed_flow_col
-    vol_df["width"] = width
-    vol_df["continuous_flow"] = continuous_flow
-    vol_df["gutter_flow"] = gutter_flow
-    vol_df["type"] = "bubbles"
+    df = pd.DataFrame()
+    df["inlet_width"] = inlet_width_col
+    df["dispersed_flow"] = dispersed_flow_col
+    df["width"] = width
+    df["continuous_flow"] = continuous_flow
+    df["gutter_flow"] = gutter_flow
+    df["type"] = "bubbles"
 
-    vol_df["epsilon"] = 0.1 * vol_df["width"]
-    vol_df["flow_ratio"] = vol_df["dispersed_flow"] / vol_df["continuous_flow"]
-    vol_df["width_ratio"] = round(vol_df["inlet_width"] / vol_df["width"], 2)
-    vol_df["width_ratio"] = vol_df["width_ratio"].astype(str)
-    vol_df["height"] = vol_df.apply(
-        lambda row: height_dictionary.get(row.width_ratio), axis=1
-    )
+    df["corner_roundness"] = 0.1 * df["width"]
+    df["flow_ratio"] = df["dispersed_flow"] / df["continuous_flow"]
+    df["width_ratio"] = round(df["inlet_width"] / df["width"], 2)
+    df["width_ratio"] = df["width_ratio"].astype(str)
+    df["height"] = df.apply(lambda row: height_dictionary.get(row.width_ratio), axis=1)
 
     # Parameters for droplets
     liq_liq_df = pd.DataFrame()
@@ -255,7 +257,7 @@ def make_fig_3(color_mapping: dict[str, str]) -> p9.ggplot:
     liq_liq_df["continuous_flow"] = continuous_flow
     liq_liq_df["gutter_flow"] = gutter_flow
 
-    liq_liq_df["epsilon"] = 0.01 * liq_liq_df["width"]
+    liq_liq_df["corner_roundness"] = 0.01 * liq_liq_df["width"]
     liq_liq_df["flow_ratio"] = (
         liq_liq_df["dispersed_flow"] / liq_liq_df["continuous_flow"]
     )
@@ -265,36 +267,31 @@ def make_fig_3(color_mapping: dict[str, str]) -> p9.ggplot:
     liq_liq_df["width_ratio"] = liq_liq_df["width_ratio"].astype(str)
     liq_liq_df["type"] = "droplets"
 
-    vol_df = pd.concat([vol_df, liq_liq_df])
+    df = pd.concat([df, liq_liq_df])
 
-    vol_df["vol"] = vol_df.apply(
+    df["vol"] = df.apply(
         lambda row: calc_nondim_total_volume(
             row.height,
             row.width,
             row.inlet_width,
-            row.epsilon,
+            row.corner_roundness,
             row.continuous_flow,
             row.dispersed_flow,
             row.gutter_flow,
         ),
         axis=1,
     )
-    vol_df["width_ratio_labs"] = vol_df.apply(
-        lambda row: "w_in/w=" + row.width_ratio, axis=1
-    )
+    df["width_ratio_labs"] = df.apply(lambda row: "w_in/w=" + row.width_ratio, axis=1)
 
-    y_max = 25
-    vol_df = vol_df[vol_df["vol"] <= y_max]
-    label_df = vol_df[vol_df["vol"] <= 25 - 2.5 * (vol_df["flow_ratio"] - 0.009)]
+    df = df[df["vol"] <= 25]
+    label_df = df[df["vol"] <= 25 - 2.5 * (df["flow_ratio"] - 0.009)]
     label_df = label_df[label_df["vol"] >= 25 - 2.5 * (label_df["flow_ratio"] + 0.009)]
     label_df["flow_ratio"][label_df["type"] == "droplets"] = 8
     plot = (
-        p9.ggplot(
-            vol_df, p9.aes("flow_ratio", "vol", color="width_ratio", linetype="type")
-        )
+        p9.ggplot(df, p9.aes("flow_ratio", "vol", color="width_ratio", linetype="type"))
         + p9.geom_line()
         + p9.scale_color_manual(color_mapping)
-        + p9.ylim(0, y_max)
+        + p9.ylim(0, 25)
         + p9.scale_x_continuous(breaks=[0, 2, 4, 6, 8, 10])
         + p9.geom_label(
             label_df,
@@ -359,7 +356,7 @@ def make_fig_6(color_mapping: dict[str, str]) -> p9.ggplot:
     df["time"] = df["alpha"] / (
         df["continuous_flow"] / (df["height"] * df["width"] ** 2)
     )
-    df["epsilon"] = 0.1 * df["width"]
+    df["corner_roundness"] = 0.1 * df["width"]
     df["width_ratio"] = round(df["inlet_width"] / df["width"], 2)
     df["width_ratio"] = df["width_ratio"].astype(str)
     df["gutter_flow"] = 0.1 * df["continuous_flow"]
@@ -368,7 +365,7 @@ def make_fig_6(color_mapping: dict[str, str]) -> p9.ggplot:
             row.height,
             row.width,
             row.inlet_width,
-            row.epsilon,
+            row.corner_roundness,
             row.continuous_flow,
             row.gutter_flow,
             row.time,
@@ -429,21 +426,12 @@ def main() -> None:
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
 
-    color_mapping = {
-        "0.33": "#CF232B",
-        "0.67": "#CF232B",
-        "1.0": "#CF232B",
-        "1.33": "#CF232B",
-        "2.0": "#CF232B",
-        "3.0": "#CF232B",
-    }
-
     print("Generating figures...")
-    fig_2a = make_fig_2a(color_mapping, calc_nondim_fill_volume)
-    fig_2a_incorrect = make_fig_2a(color_mapping, calc_incorrect_nondim_fill_volume)
-    fig_2b = make_fig_2b(color_mapping)
-    fig_3 = make_fig_3(color_mapping)
-    fig_6 = make_fig_6(color_mapping)
+    fig_2a = make_fig_2a(COLOR_MAPPING, calc_nondim_fill_volume)
+    fig_2a_incorrect = make_fig_2a(COLOR_MAPPING, calc_incorrect_nondim_fill_volume)
+    fig_2b = make_fig_2b(COLOR_MAPPING)
+    fig_3 = make_fig_3(COLOR_MAPPING)
+    fig_6 = make_fig_6(COLOR_MAPPING)
 
     print("Saving figures...")
     fig_2a.save(
